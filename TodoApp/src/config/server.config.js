24230aadaecdcb39c432dc6bb7d032cd9a8169f5
pushprajjaiswal <pushprajapp12@gmail.com>
2024-03-06 20:14:04 +0530

    main structure changes

--- TodoApp/src/config/server.config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv'); // give the sccess of packaage
-dotenv.config(); // start reading the .env file
-
-module.exports= {
-    PORT:  process.env.PORT
-    // EMAIL: process.env.EMAIL
-}
--- TodoApp/src/controllers/home.controller.js
+++
@@ -1,8 +0,0 @@
-function homePingController(req, res) {
-    // Collect the request
-    // send it to service
-    // and the prepare and send response
-    return res.json({msg:'OK'});
-}
-
-module.exports = homePingController;
